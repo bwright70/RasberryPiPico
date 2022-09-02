e85754e91ec75928a36a047c497672671cbebbfc
bwright70 <bwright70@charlottesvilleschools.org>
2022-09-02 10:43:01 -0400

    fkhfksd

---
+++ raspberry-pi/Launchpad.py
@@ -0,0 +1,21 @@
+# type: ignore
+import board
+import digitalio
+import time 
+
+led = digitalio.DigitalInOut(board.LED)
+led.direction = digitalio.Direction.OUTPUT
+Counter = 10 
+
+while True: 
+    if Counter > 0: 
+        Counter -= Counter 
+        led.value = True
+        print(Counter)
+        time.sleep(2)
+        led.value = False
+        time.sleep(2)
+ 
+
+
+    
