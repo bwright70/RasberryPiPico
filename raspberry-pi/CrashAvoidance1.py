b18f4da7662df05ce1efe5025b562d536451da0b
bwright70 <bwright70@charlottesvilleschools.org>
2022-09-16 10:44:54 -0400

    Crash Avoidance Ver 1

---
+++ raspberry-pi/CrashAvoidance1.py
@@ -0,0 +1,20 @@
+#type: ignore 
+import digitalio
+import time  
+import board 
+import adafruit_mpu6050 
+import busio 
+
+led = digitalio.DigitalInOut(board.GP28)
+led.direction = digitalio.Direction.OUTPUT 
+sda_pin = board.GP16
+scl_pin = board.GP17
+i2c = busio.I2C(scl_pin, sda_pin)
+mpu = adafruit_mpu6050.MPU6050(i2c) 
+
+while True: 
+        x = mpu.acceleration[0] 
+        y = mpu.acceleration[1] 
+        z = mpu.acceleration[2] 
+        if x > 1:
+                print(f"X = {x}") 
